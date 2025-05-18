ae2a9e410cc48378088bbd4e67254575b89628a2
jdepaula <javierantoniodepaula@gmail.com>
2025-05-18 14:25:53 -0400

    Comentando mi clase Webservice

--- src/Webservice.java
+++ src/Webservice.java
@@ -11,13 +11,18 @@ public class Webservice {
 
     public JsonObject consultar(String divisa) throws IOException {
 
+        // 1. Construcción dinámica de la URL según la divisa solicitada
         String url_str = "https://v6.exchangerate-api.com/v6/8942777d237f9e572c8d8ce5/latest/"+divisa;
 
+        // 2. Apertura de la conexión HTTP
         URL url = new URL(url_str);
         HttpURLConnection request = (HttpURLConnection) url.openConnection();
-        request.connect();
+        request.connect(); // Establece la conexión (lanza IOException si falla)
 
+        // 3. Parseo del cuerpo de la respuesta a JSON con la librería Gson
         JsonElement root = JsonParser.parseReader(new InputStreamReader(request.getInputStream()));
+
+        // 4. Se devuelve el objeto JSON
         return root.getAsJsonObject();
     }
 }
